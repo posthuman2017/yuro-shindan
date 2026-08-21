32b39e1d6c4cec5f73b9126c6abdbcb4f2c54cd5
posthuman2017 <posthuman2017@gmail.com>
2026-08-21 15:14:36 +0900

    回答送信先のエンドポイントを設定

--- js/config.js
+++ js/config.js
@@ -3,6 +3,6 @@
    回答が集計用スプレッドシートに追記され、担当者宛にメール通知されます。
    空欄のままの場合、回答は送信されず端末内のみで完結します（README参照）。 */
 
-const SUBMIT_ENDPOINT = '';
+const SUBMIT_ENDPOINT = 'https://script.google.com/macros/s/AKfycbxw4lSRjAAlvIoYy6gzkB0pxzHHql5BEdrtJ4pqMd9plQYD0rCkF0lOu-L9660_9QGRwg/exec';
 
 const SUBMIT_RECIPIENT = 'あいおいニッセイ同和損害保険株式会社';
